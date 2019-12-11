d68caa165e23b80b786925fce0bfb9b112d7a667
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-12-11 19:12:26 +0100

    Agregar archivos de proyecto.

---
+++ ScapeRoomVentilacion/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScapeRoomVentilacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string Pass { get; set; } = "";
+        private static string Usuario { get; set; } = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (PassBox.Password == Pass && UsuarioTextBox.Text == Usuario)
+            {
+                Window1 window = new Window1();
+                window.Activate();
+                window.Show();
+                this.Close();
+            }
+            if (PassBox.Password != Pass)
+            {
+                PassErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PassErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+            if (UsuarioTextBox.Text != Usuario)
+            {
+                UsuarioErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                UsuarioErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
